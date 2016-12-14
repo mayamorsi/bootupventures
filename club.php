f7d886549a326a1d5a574db5932417712ff6104a
sanjukanwasi <sanjukanwasi@gmail.com>
2016-12-14 22:49:35 +0530

    font size fixed

--- club.php
+++ club.php
@@ -19,150 +19,143 @@
 <!-- <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"> -->
 <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
 <style>
-.club, .club p {
-  font-family: Montserrat;
+.club, .club p, .club h2, .club h3 {
+	font-family: Roboto Slab !important;
+}
+.club p {
+	font-size: 17px;
 }
 .club .block1 {
-  background: url('http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/Slide_ImageHero.png') no-repeat 0 0;
-  height: 500px;
+	background: url('http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/Slide_ImageHero.png') no-repeat 0 0;
+	height: 500px;
 }
 .section-icon {
-  width: 75px;
+	width: 75px;
 }
 .feature-img {
-  width: 100%;
-  height: auto;
+	width: 100%;
+	height: auto;
 }
 .regButton {
-  padding: 10px 30px;
-}
-section.home-section-1 {
-  background-attachment: fixed;
-  background-position: 50% 0;
-  background-repeat: repeat;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  background: url(http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/Slide_ImageHero.png) no-repeat bottom center fixed;
-  background-color: #333333;
-}
-section.club-section-6 {
-  background-color: #1c1514;
-  color: #fff;
+	padding: 10px 30px;
 }
 .club-section-padding {
-  padding: 50px;
+	padding: 50px;
 }
 .club-section h2 {
-  margin-bottom: 20px;
+	margin-bottom: 20px;
+	font-size:36px;
 }
-.club-section-2, .club-section-4 {
-  color: #fff;
+.club-section-2, .club-section-4, .club-section-6 {
+	color: #fff;
 }
 .club-section-1 h2 {
-  color: #bc7642;
+	color: #bc7642;
 }
-.club-section-2 h2, .club-section-4 h2,.club-section-6 h2, .club-section-2 h3  {
-  color: #f3cc60;
+.club-section-2 h2, .club-section-4 h2, .club-section-6 h2, .club-section-2 h3 {
+	color: #f3cc60;
 }
 .club-section-2 h3 {
-  font-size: 20px;
+	font-size: 20px;
+}
+.club-section-6 {
+	background-color: #1c1514;
+	color: #fff;
 }
 </style>
 </head>
-<body class="club">
+<body>
 <?PHP include 'menu.php'; ?>
-<div class="wrapper">
-	<div class="block1">
-		<div class="rect">
-			<h2>Team</h2>
-			<p>The BootUP team consists of serial entrepreneurs, investors, corporate executives and operations experts that have come together to build one of the most powerful entrepreneurial ecosystems in the world. </p>
-		</div>
-	</div>
-	<section class="club-section club-section-padding club-section-1 text-center">
-		<div class="container">
-			<h2>The Lore Of The Cuckoo's Nest</h2>
-			<p> The inspiration for Ken Kesey's book—One Flew Over the Cuckoo's Nest—came in the early 1960s while working the night shift at the Menlo Park Veterans Hospital on Willow Road in Menlo Park. "The patients were not insane, they just did not fit the conventional idea of how people are supposed to act,” he observed. Another local, Steve Jobs, took this observation a step further, “The people who are crazy enough to think they can change the world are the ones who do.” In this spirit, the Cuckoo’s Nest is to create an experience for leaders in innovation and business who are driven by an entrepreneurial and artistic spirit with a relaxed collaborative environment for the exchange ideas and concepts to grow.</p>
-		</div>
-	</section>
-	<section class="club-section club-section-2 text-center" id="nest" style="width: 100%; min-height: 400px;  background:url(http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/Slide_Image02.png); background-size: cover; background-attachment: fixed;">
-		<div class="container">
-			<div class="club-section-padding">
-				<h2>The Nest</h2>
-				<img class="section-icon" src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/TheNest_Leaficon.png">
-				<h3>Member Values &amp; Benefits</h3>
-				<p>Key to our success will be the quality and caliber of our members. Our members have the knowledge and experience to actively contribute to others. We will provide a unique experience and programs to stimulate engagement by all members. We will strive for operational excellence in all aspects of our business. We are always profitable and we approach every aspect of the business with a positive, can do our attitude. There is a commitment to always foster and accept the spirit of disruption within the framework of good taste. As members we will abide by a code of mutual respect and trust for each other.</p>
-			</div>
-		</div>
-	</section>
-	<section class="club-section club-section-padding club-section-3 text-center" id="founding">
-		<div class="container">
-			<h2>Founding &amp; Charter Members</h2>
-			<img class="section-icon" src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/Members_Global_icon.png">
-			<h3 style="text-align: center; font-size: 20px;  color: #40140D;">Thought leaders &amp; company builders</h3>
-			<p>The Cuckoo’s Nest has attracted some of the world's most innovative thinkers and successful entrepreneurs. The total individual membership is limited 1,200 global players; of which 51 percent are female, and 20 percent are under 30. Our members share a common vision that technology innovation empowers people by giving them more access to information and opportunity.</p>
-		</div>
-	</section>
-	<div class="club-section club-section-4 text-center" id="partner" style="width: 100%; height: 400px;background:url(http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/Slide_Image.png); background-size: cover; background-attachment: fixed;">
-		<div class="container">
-			<div class="club-section-padding">
-				<h2>Founding Partners</h2>
-				<img class="section-icon" src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/Partners_Handshake_icon.png">
-				<h3 style="text-align: center; font-size: 20px;  color: #F3CC60;">Championing the entrepreneurial dream</h3>
-				<p>The Nest’s Founding Partners are the leading brands in professional services, technology, high-end consumer products, and media. Like our members, these top brands have a stake in global innovation and the companies and entrepreneurs creating our future.</p>
-			</div>
-		</div>
-	</div>
-	<section id="sponsors" class="club-section club-section-5 club-section-padding text-center">
-		<div class="container">
-			<h2>Sponsors</h2>
-			<div class="row">
-				<div class="col-md-4" style="padding:5%;">
-					<div class="img-box figcaption-middle text-center fade-in"> <a href="https://www.opusbank.com/" target="_blank"> <img src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/opus-bank-logo.jpg" alt="" class="feature-img " style="visibility: visible;"></a> </div>
-				</div>
-				<div class="col-md-4" style="padding:5%;">
-					<div class="img-box figcaption-middle text-center fade-in"> <a href="http://www.sheppardmullin.com/" target="_blank"> <img src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/sheppard-logo.jpg" alt="" class="feature-img " style="visibility: visible;"></a> </div>
-				</div>
-				<div class="col-md-4" style="padding:5%;">
-					<div class="img-box figcaption-middle text-center fade-in"><a href="https://www.morganlewis.com/" target="_blank"> <img src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/MorganLewis-logo-01.png" alt="" class="feature-img " style="visibility: visible;"></a></div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-4" style="padding:5%;">
-					<div class="img-box figcaption-middle text-center fade-in"><a href="https://www.manatt.com/" target="_blank"> <img src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/manatt_logo.jpg" alt="" class="feature-img " style="visibility: visible;"></a></div>
-				</div>
-				<div class="col-md-4" style="padding:5%;">
-					<div class="img-box figcaption-middle text-center fade-in"><a href="https://home.kpmg.com/us/en/home.html" target="_blank"> <img src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/KPMG2000x779.png" alt="" class="feature-img " style="visibility: visible;"></a></div>
-				</div>
-				<div class="col-md-4" style="padding:5%;">
-					<div class="img-box figcaption-middle text-center fade-in"><a href="http://www.luxembourgforbusiness.lu/en" target="_blank"> <img src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/Luxembourg-for-Business.png" alt="" class="feature-img " style="visibility: visible;"></a></div>
-				</div>
-			</div>
-			<a href="http://www.cuckoosnestclub.com/companies/">
-			<button type="Registerbutton" class="regButton" style="text-align: center; background: #d84100; border-style: none; color: white;">Full Portfolio</button>
-			</a> </div>
-	</section>
-	<section id="benefits" class="club-section club-section-6 club-section-padding text-center">
-		<div class="container">
-			<h2>Membership Benefits</h2>
-			<div class="row" style="width: 50%; margin-left: auto; margin-right: auto;text-align: center;">
-				<div class="col-md-12">
-					<p style="color: white; font-size: 17px;">Online access to member directory and event calendar</p>
-					<p style="color: white; font-size: 17px;">Regular member newsletter</p>
-					<p style="color: white; font-size: 17px;">Invitations to guest speakers visiting BootUP</p>
-					<p style="color: white; font-size: 17px;">Access to monthly startup pitch events</p>
-					<p style="color: white; font-size: 17px;">Behind the scenes visits to Bay Area companies</p>
-					<p style="color: white; font-size: 17px;">Participate in a variety of social and cultural events</p>
-					<p style="color: white; font-size: 17px;">Hot desking at BootUP in Menlo Park</p>
-					<p style="color: white; font-size: 17px;">Host business meetings in a relaxed garden setting</p>
-				</div>
-			</div>
-			<br/>
-			<a href="http://www.cuckoosnestclub.com/cuckoos-nest-membership/">
-			<button type="Registerbutton" class="regButton" style="text-align: center; background: #d84100; border-style: none; color: white;">Register Now</button>
-			</a> </div>
-	</section>
+<div class="wrapper club">
+ <div class="block1">
+  <div class="rect">
+   <h2>Team</h2>
+   <p>The BootUP team consists of serial entrepreneurs, investors, corporate executives and operations experts that have come together to build one of the most powerful entrepreneurial ecosystems in the world. </p>
+  </div>
+ </div>
+ <section class="club-section club-section-padding club-section-1 text-center">
+  <div class="container">
+   <h2>The Lore Of The Cuckoo's Nest</h2>
+   <p> The inspiration for Ken Kesey's book—One Flew Over the Cuckoo's Nest—came in the early 1960s while working the night shift at the Menlo Park Veterans Hospital on Willow Road in Menlo Park. "The patients were not insane, they just did not fit the conventional idea of how people are supposed to act,” he observed. Another local, Steve Jobs, took this observation a step further, “The people who are crazy enough to think they can change the world are the ones who do.” In this spirit, the Cuckoo’s Nest is to create an experience for leaders in innovation and business who are driven by an entrepreneurial and artistic spirit with a relaxed collaborative environment for the exchange ideas and concepts to grow.</p>
+  </div>
+ </section>
+ <section class="club-section club-section-2 text-center" id="nest" style="width: 100%; min-height: 400px;  background:url(http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/Slide_Image02.png); background-size: cover; background-attachment: fixed;">
+  <div class="container">
+   <div class="club-section-padding">
+    <h2>The Nest</h2>
+    <img class="section-icon" src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/TheNest_Leaficon.png">
+    <h3>Member Values &amp; Benefits</h3>
+    <p>Key to our success will be the quality and caliber of our members. Our members have the knowledge and experience to actively contribute to others. We will provide a unique experience and programs to stimulate engagement by all members. We will strive for operational excellence in all aspects of our business. We are always profitable and we approach every aspect of the business with a positive, can do our attitude. There is a commitment to always foster and accept the spirit of disruption within the framework of good taste. As members we will abide by a code of mutual respect and trust for each other.</p>
+   </div>
+  </div>
+ </section>
+ <section class="club-section club-section-padding club-section-3 text-center" id="founding">
+  <div class="container">
+   <h2>Founding &amp; Charter Members</h2>
+   <img class="section-icon" src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/Members_Global_icon.png">
+   <h3 style="text-align: center; font-size: 20px;  color: #40140D;">Thought leaders &amp; company builders</h3>
+   <p>The Cuckoo’s Nest has attracted some of the world's most innovative thinkers and successful entrepreneurs. The total individual membership is limited 1,200 global players; of which 51 percent are female, and 20 percent are under 30. Our members share a common vision that technology innovation empowers people by giving them more access to information and opportunity.</p>
+  </div>
+ </section>
+ <div class="club-section club-section-4 text-center" id="partner" style="width: 100%; height: 400px;background:url(http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/Slide_Image.png); background-size: cover; background-attachment: fixed;">
+  <div class="container">
+   <div class="club-section-padding">
+    <h2>Founding Partners</h2>
+    <img class="section-icon" src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/Partners_Handshake_icon.png">
+    <h3 style="text-align: center; font-size: 20px;  color: #F3CC60;">Championing the entrepreneurial dream</h3>
+    <p>The Nest’s Founding Partners are the leading brands in professional services, technology, high-end consumer products, and media. Like our members, these top brands have a stake in global innovation and the companies and entrepreneurs creating our future.</p>
+   </div>
+  </div>
+ </div>
+ <section id="sponsors" class="club-section club-section-5 club-section-padding text-center">
+  <div class="container">
+   <h2>Sponsors</h2>
+   <div class="row">
+    <div class="col-md-4" style="padding:5%;">
+     <div class="img-box figcaption-middle text-center fade-in"> <a href="https://www.opusbank.com/" target="_blank"> <img src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/opus-bank-logo.jpg" alt="" class="feature-img " style="visibility: visible;"></a> </div>
+    </div>
+    <div class="col-md-4" style="padding:5%;">
+     <div class="img-box figcaption-middle text-center fade-in"> <a href="http://www.sheppardmullin.com/" target="_blank"> <img src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/sheppard-logo.jpg" alt="" class="feature-img " style="visibility: visible;"></a> </div>
+    </div>
+    <div class="col-md-4" style="padding:5%;">
+     <div class="img-box figcaption-middle text-center fade-in"><a href="https://www.morganlewis.com/" target="_blank"> <img src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/MorganLewis-logo-01.png" alt="" class="feature-img " style="visibility: visible;"></a></div>
+    </div>
+   </div>
+   <div class="row">
+    <div class="col-md-4" style="padding:5%;">
+     <div class="img-box figcaption-middle text-center fade-in"><a href="https://www.manatt.com/" target="_blank"> <img src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/manatt_logo.jpg" alt="" class="feature-img " style="visibility: visible;"></a></div>
+    </div>
+    <div class="col-md-4" style="padding:5%;">
+     <div class="img-box figcaption-middle text-center fade-in"><a href="https://home.kpmg.com/us/en/home.html" target="_blank"> <img src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/KPMG2000x779.png" alt="" class="feature-img " style="visibility: visible;"></a></div>
+    </div>
+    <div class="col-md-4" style="padding:5%;">
+     <div class="img-box figcaption-middle text-center fade-in"><a href="http://www.luxembourgforbusiness.lu/en" target="_blank"> <img src="http://cuckoosnest.afamilyfriendgroup.org/wp-content/uploads/2016/10/Luxembourg-for-Business.png" alt="" class="feature-img " style="visibility: visible;"></a></div>
+    </div>
+   </div>
+   <a href="http://www.cuckoosnestclub.com/companies/">
+   <button type="Registerbutton" class="regButton" style="text-align: center; background: #d84100; border-style: none; color: white;">Full Portfolio</button>
+   </a> </div>
+ </section>
+ <section id="benefits" class="club-section club-section-6 club-section-padding text-center">
+  <div class="container">
+   <h2>Membership Benefits</h2>
+   <div class="row" style="width: 50%; margin-left: auto; margin-right: auto;text-align: center;">
+    <div class="col-md-12">
+     <p>Online access to member directory and event calendar</p>
+     <p>Regular member newsletter</p>
+     <p>Invitations to guest speakers visiting BootUP</p>
+     <p>Access to monthly startup pitch events</p>
+     <p>Behind the scenes visits to Bay Area companies</p>
+     <p>Participate in a variety of social and cultural events</p>
+     <p>Hot desking at BootUP in Menlo Park</p>
+     <p>Host business meetings in a relaxed garden setting</p>
+    </div>
+   </div>
+   <br/>
+   <a href="http://www.cuckoosnestclub.com/cuckoos-nest-membership/">
+   <button type="Registerbutton" class="regButton" style="text-align: center; background: #d84100; border-style: none; color: white;">Register Now</button>
+   </a> </div>
+ </section>
 </div>
 <?PHP include 'footer.php'; ?>
 <!-- jQuery --> 
